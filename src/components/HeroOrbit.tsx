280869ade4494af4c22238b963ef632c0ce4dbaf
Gandhi Balingkit <balingkitgandhi@gmail.com>
2024-10-05 09:52:32 +0800

    Done hero animation

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,21 +1,53 @@
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
-const HeroOrbit = ({ children, size, rotation }: PropsWithChildren<{size: number, rotation: number}>) => {
+const HeroOrbit = ({
+  children,
+  size,
+  rotation,
+  shouldOrbit = false,
+  orbitDuration,
+  shouldSpin = false,
+  spinDuration,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  shouldOrbit: boolean;
+  orbitDuration?: string;
+  shouldSpin: boolean;
+  spinDuration?: string;
+}>) => {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div 
-            className="flex items-start justify-start" 
-            style={{width: `${size}px`, height: `${size}px`, transform: `rotate(${rotation}deg)`}}
+      <div
+        className={shouldOrbit ? true && "animate-spin" : ""}
+        style={{
+          animationDuration: orbitDuration,
+        }}
+      >
+        <div
+          className="flex items-start justify-start"
+          style={{
+            width: `${size}px`,
+            height: `${size}px`,
+            transform: `rotate(${rotation}deg)`,
+          }}
         >
-          <div 
-            className="inline-flex"
-            style={{transform: `rotate(${rotation * -1}deg)`}}
+          <div
+            className={shouldSpin ? true && "animate-spin" : ""}
+            style={{ animationDuration: spinDuration }}
           >
-            {children}
+            <div
+              className="inline-flex"
+              style={{ transform: `rotate(${rotation * -1}deg)` }}
+            >
+              {children}
+            </div>
           </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroOrbit
+export default HeroOrbit;
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,71 +6,130 @@ import HeroOrbit from "@/components/HeroOrbit";
 import StarIcon from "@/assets/icons/star.svg";
 import SparKleIcon from "@/assets/icons/sparkle.svg";
 
-
-
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
       <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
-        <div 
-          className="absolute inset-0 -z-30 opacity-5" 
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
           style={{
-            backgroundImage: `url(${GrainImage.src})`
+            backgroundImage: `url(${GrainImage.src})`,
           }}
         ></div>
         <div className="hero-ring"></div>
         <div className="size-[820px] hero-ring"></div>
         <div className="size-[1020px] hero-ring"></div>
         <div className="size-[1220px] hero-ring"></div>
-        <HeroOrbit size={800} rotation={-72}>
-          <StarIcon className="size-28 text-emerald-300"/>
+
+        <HeroOrbit
+          size={430}
+          rotation={-14}
+          shouldOrbit
+          orbitDuration="30s"
+          shouldSpin
+          spinDuration="3s"
+        >
+          <SparKleIcon className="size-8 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={550} rotation={20}>
-          <StarIcon className="size-12 text-emerald-300"/>
+        <HeroOrbit
+          size={440}
+          rotation={79}
+          shouldOrbit
+          orbitDuration="32s"
+          shouldSpin
+          spinDuration="3s"
+        >
+          <SparKleIcon className="size-5 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={590} rotation={98}>
-          <StarIcon className="size-8 text-emerald-300"/>
+        <HeroOrbit size={520} rotation={-41} shouldOrbit orbitDuration="34s">
+          <div className="size-2 bg-emerald-300/20 rounded-full" />
         </HeroOrbit>
-        <HeroOrbit size={430} rotation={-14}>
-          <SparKleIcon className="size-8 text-emerald-300/20"/>
+        <HeroOrbit
+          size={530}
+          rotation={178}
+          shouldOrbit
+          orbitDuration="36s"
+          shouldSpin
+          spinDuration="3s"
+        >
+          <SparKleIcon className="size-10 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={440} rotation={79}>
-          <SparKleIcon className="size-5 text-emerald-300/20"/>
+        <HeroOrbit
+          size={550}
+          rotation={20}
+          shouldOrbit
+          orbitDuration="38s"
+          shouldSpin
+          spinDuration="6s"
+        >
+          <StarIcon className="size-12 text-emerald-300" />
         </HeroOrbit>
-        <HeroOrbit size={530} rotation={178}>
-          <SparKleIcon className="size-10 text-emerald-300/20"/>
+        <HeroOrbit
+          size={590}
+          rotation={98}
+          shouldOrbit
+          orbitDuration="40s"
+          shouldSpin
+          spinDuration="6s"
+        >
+          <StarIcon className="size-8 text-emerald-300" />
         </HeroOrbit>
-        <HeroOrbit size={710} rotation={144}>
-          <SparKleIcon className="size-14 text-emerald-300/20"/>
+        <HeroOrbit size={650} rotation={-5} shouldOrbit orbitDuration="42s">
+          <div className="size-2 bg-emerald-300/20 rounded-full" />
         </HeroOrbit>
-        <HeroOrbit size={720} rotation={85}>
-          <div className="size-3 bg-emerald-300/20 rounded-full"/>
+        <HeroOrbit
+          size={710}
+          rotation={144}
+          shouldOrbit
+          orbitDuration="44s"
+          shouldSpin
+          spinDuration="6s"
+        >
+          <SparKleIcon className="size-14 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={520} rotation={-41}>
-          <div className="size-2 bg-emerald-300/20 rounded-full"/>
+        <HeroOrbit size={720} rotation={85} shouldOrbit orbitDuration="46s">
+          <div className="size-3 bg-emerald-300/20 rounded-full" />
         </HeroOrbit>
-        <HeroOrbit size={650} rotation={-5}>
-          <div className="size-2 bg-emerald-300/20 rounded-full"/>
+        <HeroOrbit
+          size={800}
+          rotation={-72}
+          shouldOrbit
+          orbitDuration="48s"
+          shouldSpin
+          spinDuration="6s"
+        >
+          <StarIcon className="size-28 text-emerald-300" />
         </HeroOrbit>
       </div>
       <div className="container">
         <div className="flex flex-col items-center">
-          <Image src={memojiImage} className="size-[100px]" alt="Person peeking behind laptop"/>
+          <Image
+            src={memojiImage}
+            className="size-[100px]"
+            alt="Person peeking behind laptop"
+          />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <div className="bg-green-500 size-2.5 rounded-full"></div>
-            <div className="text-sm font-medium">Available for new projects</div>
+            <div className="bg-green-500 size-2.5 rounded-full relative">
+              <div className="bg-green-500 absolute inset-0 animate-ping-large rounded-full"></div>
+            </div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
           </div>
         </div>
         <div className="max-w-lg mx-auto">
-          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building Exceptional User Experiences
+          </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
-            I specialize in transforming designs into functional, high-performing web applications. Let's discuss your next project.
+            I specialize in transforming designs into functional,
+            high-performing web applications. Let's discuss your next project.
           </p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
             <span className="font-semibold">Explore My Work</span>
-            <ArrowDown className="size-4"/>
+            <ArrowDown className="size-4" />
           </button>
           <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
             <span>👋</span>
