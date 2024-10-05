75e4d27b504aa9954cfa5cc7ce85634e46ffbe31
Gandhi Balingkit <balingkitgandhi@gmail.com>
2024-10-05 12:58:10 +0800

    Update the header background color

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
 
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
-      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+      <nav className="flex gap-1 p-0.5 border border-white/20 rounded-full bg-white/20 backdrop-blur">
         <a
           className="nav-item"
           data-section="home"
