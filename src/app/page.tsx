370c7ed895c822b83a1a641fb1a8729a5674b305
Gandhi Balingkit <balingkitgandhi@gmail.com>
2024-10-05 12:51:20 +0800

    Add functionality to the navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
@@ -6,17 +8,35 @@ import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
 import { Footer } from "@/sections/Footer";
+import { useEffect } from "react";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <HeroSection />
-      <ProjectsSection />
+
+      <section id="home">
+        <HeroSection />
+      </section>
+
+      <section id="pro">
+        <ProjectsSection />
+      </section>
+
       <TapeSection />
-      <TestimonialsSection />
-      <AboutSection />
-      <ContactSection />
+
+      <section id="testimonials">
+        <TestimonialsSection />
+      </section>
+
+      <section id="about">
+        <AboutSection />
+      </section>
+
+      <section id="contact">
+        <ContactSection />
+      </section>
+
       <Footer />
     </div>
   );
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,19 +1,44 @@
 export const Header = () => {
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    section?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <a href="#" className="nav-item">
+        <a
+          className="nav-item"
+          data-section="home"
+          onClick={() => scrollToSection("home")}
+        >
           Home
         </a>
-        <a href="#" className="nav-item">
+        <a
+          className="nav-item"
+          data-section="pro"
+          onClick={() => scrollToSection("pro")}
+        >
           Projects
         </a>
-        <a href="#" className="nav-item">
+        <a
+          className="nav-item"
+          data-section="testimonials"
+          onClick={() => scrollToSection("testimonials")}
+        >
+          Testimonials
+        </a>
+        <a
+          className="nav-item"
+          data-section="about"
+          onClick={() => scrollToSection("about")}
+        >
           About
         </a>
         <a
-          href="#"
-          className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
+          className="nav-item"
+          data-section="contact"
+          onClick={() => scrollToSection("contact")}
         >
           Contact
         </a>
